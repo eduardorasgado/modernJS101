e7ccfc677d4137b6a4a2437c0a4af037f026fe9b
lal <eduardo.rasgado@hotmail.com>
2018-07-25 07:02:49 -0500

    button toggable lesson

---
+++ intermediate/p1/index.js
@@ -0,0 +1,25 @@
+class Toggable {
+	constructor(element) {
+		//inicializador
+		this.element = element
+		this.element.innerHTML = "Off"
+		this.activated = false
+		//necesario para que js no tome como this al objeto boton del html
+		this.element.addEventListener('click', this.onClick.bind(this))
+	}
+
+	onClick () {
+		//cambiar el estado interno
+		//lamaos a toggleText
+		this.activated = !this.activated
+		this.toggleText()
+	}
+	toggleText () {
+		//cambiar el texto
+		this.element.innerHTML = this.activated ? 'On' : 'Off'
+	}
+}
+
+const button = document.getElementById("boton")
+
+const miBoton = new Toggable(button)
